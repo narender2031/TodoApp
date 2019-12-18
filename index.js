cdb1bb8c37f9886cfeba98942b0e717029fc66c4
Narender Singh <narender2031@gmail.com>
2019-12-19 00:15:28 +0530

    realm config and basic example

--- index.js
+++ index.js
@@ -2,8 +2,31 @@
  * @format
  */
 
+import * as React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
+import {Provider as PaperProvider, DefaultTheme} from 'react-native-paper';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#f7b731',
+    accent: '#82ccdd',
+    text: '#1e272e',
+    background: '#ffffff',
+    placeholder: '#778ca3',
+    disabled: '#dfe6e9',
+  },
+};
+
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
